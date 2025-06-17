a5783ceeb0151ea413ba3e9342dfbfd81435867b
AlessandroPhdNuclearAstrophysics <ale.alessandro@gmail.com>
2025-06-17 18:36:43 +0200

    Enhance quantum number utilities by adding new routines for isospin conversion and potential calculations, and improve module documentation for clarity. improved potential  calls

--- src/libs/potential_pw.f90
+++ src/libs/potential_pw.f90
@@ -1,33 +1,50 @@
 !> \file potential_pw.f90
-!! \brief Interfaccia per il calcolo dei potenziali nucleari a onde parziali.
+!! \brief Interface for the calculation of partial-wave nuclear potentials.
 !!
-!! Questo modulo fornisce la subroutine POT_PW per il calcolo della matrice
-!! del potenziale nucleone-nucleone in diversi modelli (AV14, AV18, EFT_PLESS, ...).
+!! This module provides interfaces and routines to compute the nucleon-nucleon
+!! potential matrix for various models (AV14, AV18, EFT_PLESS, ...).
 !!
 !! \author Alessandro Grassi
 !! \date 2025
+
 MODULE POTENTIALS
+  USE QUANTUM_NUMBERS
   IMPLICIT NONE
-  !> Espone la subroutine per il calcolo del potenziale a onde parziali.
+
   PUBLIC :: POT_PW
+  PUBLIC :: POT_PW_PARAMS_ALL
+  PUBLIC :: POT_PW_PARAMS
+  PUBLIC :: POT_PW_PARAMS_CHANNEL
+  PUBLIC :: POTENTIAL_PARAMETERS
+
+  INTERFACE POT_PW
+    MODULE PROCEDURE POT_PW_PARAMS_ALL
+    MODULE PROCEDURE POT_PW_PARAMS
+    MODULE PROCEDURE POT_PW_PARAMS_CHANNEL
+  END INTERFACE
+
+  !> Structure containing all parameters needed to specify a nuclear potential.
+  TYPE:: POTENTIAL_PARAMETERS
+    INTEGER :: POT_MODEL      = 0   !< Potential identifier (14=AV14, 18=AV18, 19=EFT_PLESS, 21=EFT_PLESS_FITTED)
+    INTEGER :: POT_SUBMODEL   = 0   !< Channel index (used by some potentials)
+    INTEGER :: EM_INTERACTION = -1  !< Type of electromagnetic interaction (0=Coulomb, >0=Check AV18 EMPOT)
+  END TYPE POTENTIAL_PARAMETERS
+
 CONTAINS
 
-  !> Calcola la matrice del potenziale nucleare a onde parziali per diversi modelli.
-  !>
-  !> In base al valore di IPOT seleziona il modello di potenziale desiderato e
-  !> richiama la relativa subroutine di calcolo.
-  !>
-  !> \param[in] IPOT Identificatore del potenziale (14=AV14, 18=AV18, 19=EFT_PLESS, 21=EFT_PLESS_FITTED)
-  !> \param[in] ILB Indice del canale (usato da alcuni potenziali)
-  !> \param[in] LEMP Tipo di interazione elettromagnetica (0=nessuna, >0=presente)
-  !> \param[in] L Momento angolare orbitale
-  !> \param[in] S Momento di spin totale
-  !> \param[in] J Momento angolare totale
-  !> \param[in] T1Z Proiezione isospin nucleone 1 (+1=protone, -1=neutrone)
-  !> \param[in] T2Z Proiezione isospin nucleone 2 (+1=protone, -1=neutrone)
-  !> \param[in] R Distanza interparticellare (fm)
-  !> \param[out] VPW Matrice 2x2 del potenziale in base accoppiata (canali misti)
-  SUBROUTINE POT_PW(IPOT, ILB, LEMP, L, S, J, T1Z, T2Z, R, VPW)
+  !> Computes the partial-wave nuclear potential matrix for different models.
+  !! Selects the desired potential model based on IPOT and calls the corresponding routine.
+  !! \param[in] IPOT Potential identifier (14=AV14, 18=AV18, 19=EFT_PLESS, 21=EFT_PLESS_FITTED)
+  !! \param[in] ILB Channel index (used by some potentials)
+  !! \param[in] LEMP Type of electromagnetic interaction (0=none, >0=present)
+  !! \param[in] L Orbital angular momentum
+  !! \param[in] S Total spin
+  !! \param[in] J Total angular momentum
+  !! \param[in] T1Z Isospin projection nucleon 1 (+1=proton, -1=neutron)
+  !! \param[in] T2Z Isospin projection nucleon 2 (+1=proton, -1=neutron)
+  !! \param[in] R Interparticle distance (fm)
+  !! \param[out] VPW 2x2 potential matrix in coupled basis (mixed channels)
+  SUBROUTINE POT_PW_PARAMS_ALL(IPOT, ILB, LEMP, L, S, J, T1Z, T2Z, R, VPW)
     USE AV18
     USE AV14
     USE EFT_PLESS
@@ -39,7 +56,8 @@ CONTAINS
   !
     INTEGER :: T
   !
-    T = MOD( MOD(L+S,2) + 1 , 2)
+    T = T_FROM_L_S(L, S)
+    VPW = 0.D0
   !
     SELECT CASE (IPOT)
     CASE (14)
@@ -57,6 +75,62 @@ CONTAINS
       STOP "ERROR, POTENTIAL NOT FOUND"
     END SELECT
     RETURN
+  END SUBROUTINE POT_PW_PARAMS_ALL
 
-  END SUBROUTINE POT_PW
+  !> Computes the partial-wave potential matrix using a parameter structure.
+  !! \param[in] POTENTIAL_PARAMS Structure containing all potential parameters
+  !! \param[in] L Orbital angular momentum
+  !! \param[in] S Total spin
+  !! \param[in] J Total angular momentum
+  !! \param[in] T1Z Isospin projection nucleon 1
+  !! \param[in] T2Z Isospin projection nucleon 2
+  !! \param[in] R Interparticle distance (fm)
+  !! \param[out] VPW 2x2 potential matrix
+  SUBROUTINE POT_PW_PARAMS(POTENTIAL_PARAMS, L, S, J, T1Z, T2Z, R, VPW)
+    IMPLICIT NONE
+    TYPE(POTENTIAL_PARAMETERS), INTENT(IN) :: POTENTIAL_PARAMS
+    INTEGER, INTENT(IN) :: L, S, J, T1Z, T2Z
+    DOUBLE PRECISION, INTENT(IN) :: R
+    DOUBLE PRECISION, INTENT(OUT) :: VPW(2,2)
+
+    CALL POT_PW_PARAMS_ALL(POTENTIAL_PARAMS%POT_MODEL, POTENTIAL_PARAMS%POT_SUBMODEL, POTENTIAL_PARAMS%EM_INTERACTION, &
+             L, S, J, T1Z, T2Z, R, VPW)
+  END SUBROUTINE POT_PW_PARAMS
+
+  !> Computes the partial-wave potential matrix for a given scattering channel.
+  !! \param[in] POTENTIAL_PARAMS Structure containing all potential parameters
+  !! \param[in] CHANNEL Scattering channel structure
+  !! \param[in] R Interparticle distance (fm)
+  !! \param[out] VPW 2x2 potential matrix
+  SUBROUTINE POT_PW_PARAMS_CHANNEL(POTENTIAL_PARAMS, CHANNEL, R, VPW)
+    IMPLICIT NONE
+    TYPE(POTENTIAL_PARAMETERS), INTENT(IN) :: POTENTIAL_PARAMS
+    TYPE(SCATTERING_CHANNEL), INTENT(IN) :: CHANNEL
+    DOUBLE PRECISION, INTENT(IN) :: R
+    DOUBLE PRECISION, INTENT(OUT) :: VPW(2,2)
+    INTEGER :: L, S, J, T1Z, T2Z, TZ, ICH
+    DOUBLE PRECISION :: VPW_(2,2)
+    
+    IF (IS_CHANNEL_COUPLED(CHANNEL)) THEN
+      J = GET_CHANNEL_J(CHANNEL)
+      TZ= GET_CHANNEL_TZ(CHANNEL)
+      CALL TZ_TO_T1Z_T2Z(TZ, T1Z, T2Z)
+      L = GET_CHANNEL_L(CHANNEL,1)
+      S = GET_CHANNEL_S(CHANNEL,1)
+      CALL POT_PW_PARAMS_ALL(POTENTIAL_PARAMS%POT_MODEL, POTENTIAL_PARAMS%POT_SUBMODEL, POTENTIAL_PARAMS%EM_INTERACTION, &
+                  L, S, J, T1Z, T2Z, R, VPW)
+    ELSE
+      VPW_ = 0.D0
+      DO ICH = 1, GET_CHANNEL_NCH(CHANNEL)
+        J = GET_CHANNEL_J(CHANNEL)
+        TZ = GET_CHANNEL_TZ(CHANNEL)
+        CALL TZ_TO_T1Z_T2Z(TZ, T1Z, T2Z)
+        L = GET_CHANNEL_L(CHANNEL, ICH)
+        S = GET_CHANNEL_S(CHANNEL, ICH)
+        CALL POT_PW_PARAMS_ALL(POTENTIAL_PARAMS%POT_MODEL, POTENTIAL_PARAMS%POT_SUBMODEL, POTENTIAL_PARAMS%EM_INTERACTION, &
+                 L, S, J, T1Z, T2Z, R, VPW_)
+        VPW(ICH, ICH) = VPW_(1,1)
+      END DO
+    END IF
+  END SUBROUTINE POT_PW_PARAMS_CHANNEL
 END MODULE POTENTIALS
