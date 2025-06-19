22faa06525a1a79143cff995bafc2376a580da39
AlessandroPhdNuclearAstrophysics <ale.alessandro@gmail.com>
2025-06-19 15:29:45 +0200

    Included zero energy evaluation for EFT_pless 10 and 15

--- src/main_scattering_zero_energy.f90
+++ src/main_scattering_zero_energy.f90
@@ -12,18 +12,16 @@ PROGRAM SCATTERING_NN_ZERO_ENERGY
   INTEGER :: IPOT, ILB, LEMP=0
   INTEGER :: L, DIM
 
-  IPOT = 18
-  ILB = 1
-  
   CALL PREPARE_CHANNELS(LMAX, JMAX, TZ, CHANNELS)
   ALLOCATE(PS(SIZE(CHANNELS), 1))
 
+  WRITE(*,*) 
+  WRITE(*,*) "===================== AV18 =========================="
+  WRITE(*,*) 
+  IPOT = 18
+  ILB = 1
+  
   ENERGIES(1) = 0.D0
-  CALL SET_CHANNEL(CHANNELS(1), 0, 0, 0, 0)
-  CALL SET_CHANNEL(CHANNELS(2), 1, 0, 1, 0)
-  CALL SET_CHANNEL(CHANNELS(3), 1, 1, 0, 0)
-  CALL SET_CHANNEL(CHANNELS(4), 0, 1, 1, 0)
-
   CALL SET_ENERGIES(ENERGIES)
   CALL SET_CHANNELS(CHANNELS)
   
@@ -86,9 +84,6 @@ PROGRAM SCATTERING_NN_ZERO_ENERGY
   OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
   WRITE(*,*) "a_1", OBS%a1
   WRITE(*,'(A,F10.3)') " Expected scattering length: ", -1/0.135085
-
-
-
   
 
   WRITE(*,*)
@@ -148,6 +143,267 @@ PROGRAM SCATTERING_NN_ZERO_ENERGY
   WRITE(*,'(A,F10.3)') " Expected scattering length L=1: ", -1/3.413315
   WRITE(*,'(A,F10.3)') " Expected mixing angle constant: ", 1.61436 
   WRITE(*,'(A,F10.3)') " Expected scattering length L=3: ", -1/1.03225
+
+
+  CALL RESET_SCATTERING_NN_VARIATIONAL
+  WRITE(*,*) 
+  WRITE(*,*) "===================== EFT_pless_15 =========================="
+  WRITE(*,*) 
+  IPOT = 19
+  ILB = 15
+  
+  CALL SET_ENERGIES(ENERGIES)
+  CALL SET_CHANNELS(CHANNELS)
+  
+  CALL NN_SCATTERING_VARIATIONAL_ENERGIES_CHANNELS(ENERGIES, CHANNELS, LEMP, PS, IPOT=IPOT, ILB=ILB)
+  
+  WRITE(*,*)
+  WRITE(*,*)
+  WRITE(*,*) CHAR(27)//'[34m'//"SINGLE CHANNEL RESULTS"//CHAR(27)//'[0m'
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(1))
+  R = PS(1,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(1), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(1))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,'(A,F10.3)') " Expected scattering length: ", -1/0.043617
+
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(3))
+  R = PS(3,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(3), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(3))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,'(A,F10.3)') " Expected scattering length: ", 1/0.581388
+
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(4))
+  R = PS(4,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(4), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(4))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,'(A,F10.3)') " Expected scattering length: ", -1/0.438454
+
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(5))
+  R = PS(5,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(5), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(5))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,'(A,F10.3)') " Expected scattering length: ", 1/0.717147
+
+
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(7))
+  R = PS(7,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(7), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(7))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,'(A,F10.3)') " Expected scattering length: ", -1/2.795050
+
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(8))
+  R = PS(8,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(8), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(8))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,'(A,F10.3)') " Expected scattering length: ", -1/1.032862
+
+  WRITE(*,*)
+  WRITE(*,*)
+  WRITE(*,*) CHAR(27)//'[34m'//"BLATT-BIEDERMANN PARAMETRIZATION"//CHAR(27)//'[0m'
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(2))
+  R = PS(2,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(2), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(2))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,*) "e  ", OBS%e
+  WRITE(*,*) "a_2", OBS%a2
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=0: ", 1/0.185051
+  WRITE(*,'(A,F10.3)') " Expected mixing angle constant: ", 0.284509
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=2: ", 1/0.829435
+  
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(6))
+  R = PS(6,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(6), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(6))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,*) "e  ", OBS%e
+  WRITE(*,*) "a_2", OBS%a2
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=1: ", -1/2.528886
+  WRITE(*,'(A,F10.3)') " Expected mixing angle constant: ", -1.63131
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=3: ", 1/1.02352
+
+  WRITE(*,*)
+  WRITE(*,*)
+  WRITE(*,*) CHAR(27)//'[34m'//"STAPP PARAMETRIZATION"//CHAR(27)//'[0m'
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(2))
+  R = PS(2,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(2), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(2))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_STAPP(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,*) "e  ", OBS%e
+  WRITE(*,*) "a_2", OBS%a2
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=0: ", 1/0.185051
+  WRITE(*,'(A,F10.3)') " Expected mixing angle constant: ", 1.53556
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=2: ", 1/0.608997
+
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(6))
+  R = PS(6,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(6), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(6))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_STAPP(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,*) "e  ", OBS%e
+  WRITE(*,*) "a_2", OBS%a2
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=1: ", -1/2.528886
+  WRITE(*,'(A,F10.3)') " Expected mixing angle constant: ", 0.644972
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=3: ", -1/14.548
+
+
+
+
+  CALL RESET_SCATTERING_NN_VARIATIONAL
+  WRITE(*,*) 
+  WRITE(*,*) "===================== EFT_pless_10 =========================="
+  WRITE(*,*) 
+  IPOT = 19
+  ILB = 10
+  
+  CALL SET_ENERGIES(ENERGIES)
+  CALL SET_CHANNELS(CHANNELS)
+  
+  CALL NN_SCATTERING_VARIATIONAL_ENERGIES_CHANNELS(ENERGIES, CHANNELS, LEMP, PS, IPOT=IPOT, ILB=ILB)
+  
+  WRITE(*,*)
+  WRITE(*,*)
+  WRITE(*,*) CHAR(27)//'[34m'//"SINGLE CHANNEL RESULTS"//CHAR(27)//'[0m'
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(1))
+  R = PS(1,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(1), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(1))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,'(A,F10.3)') " Expected scattering length: ", -1/0.043585
+
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(3))
+  R = PS(3,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(3), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(3))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,'(A,F10.3)') " Expected scattering length: ", 1/0.583100
+
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(4))
+  R = PS(4,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(4), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(4))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,'(A,F10.3)') " Expected scattering length: ", -1/0.466579
+
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(5))
+  R = PS(5,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(5), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(5))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,'(A,F10.3)') " Expected scattering length: ", 1/0.749634
+
+
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(7))
+  R = PS(7,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(7), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(7))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,'(A,F10.3)') " Expected scattering length: ", -1/2.664854
+
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(8))
+  R = PS(8,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(8), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(8))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,'(A,F10.3)') " Expected scattering length: ", -1/1.124504
+
+
+  WRITE(*,*)
+  WRITE(*,*)
+  WRITE(*,*) CHAR(27)//'[34m'//"BLATT-BIEDERMANN PARAMETRIZATION"//CHAR(27)//'[0m'
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(2))
+  R = PS(2,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(2), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(2))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,*) "e  ", OBS%e
+  WRITE(*,*) "a_2", OBS%a2
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=0: ", 1/0.1849
+  WRITE(*,'(A,F10.3)') " Expected mixing angle constant: ", 0.285121
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=2: ", 1/0.878179
+
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(6))
+  R = PS(6,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(6), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(6))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_BB(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,*) "e  ", OBS%e
+  WRITE(*,*) "a_2", OBS%a2
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=1: ", -1/2.825603
+  WRITE(*,'(A,F10.3)') " Expected mixing angle constant: ", -1.66873
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=3: ", 1/1.08302
+
+  WRITE(*,*)
+  WRITE(*,*)
+  WRITE(*,*) CHAR(27)//'[34m'//"STAPP PARAMETRIZATION"//CHAR(27)//'[0m'
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(2))
+  R = PS(2,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(2), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(2))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_STAPP(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,*) "e  ", OBS%e
+  WRITE(*,*) "a_2", OBS%a2
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=0: ", 1/0.1849
+  WRITE(*,'(A,F10.3)') " Expected mixing angle constant: ", 1.5402 
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=2: ", 1/0.633951
+
+  WRITE(*,*)
+  WRITE(*,*) "Scattering lengths at zero energy for channel: ", GET_CHANNEL_NAME(CHANNELS(6))
+  R = PS(6,1)%R_BB
+  L = GET_CHANNEL_L(CHANNELS(6), 1)
+  DIM = GET_CHANNEL_NCH(CHANNELS(6))
+  OBS = EVALUATE_ZERO_ENERGIES_OBSERVABLES_STAPP(R, DIM, L)
+  WRITE(*,*) "a_1", OBS%a1
+  WRITE(*,*) "e  ", OBS%e
+  WRITE(*,*) "a_2", OBS%a2
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=1: ", -1/2.825603
+  WRITE(*,'(A,F10.3)') " Expected mixing angle constant: ", 0.590358
+  WRITE(*,'(A,F10.3)') " Expected scattering length L=3: ", -1/14.1517
 
   DEALLOCATE(CHANNELS)
   DEALLOCATE(PS)
